f786336dad6746dce075e0df5d84c3f399474fd1
Dimas <dimasradhitya9@gmail.com>
2020-12-05 00:08:30 +0700

    Part 6

--- wtfforms_fields.py
+++ wtfforms_fields.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+
+from models import User
 
 class RegistrationForm(FlaskForm):
     """ Registration Form """
@@ -15,3 +17,8 @@ class RegistrationForm(FlaskForm):
         validators=[InputRequired(message="Username Required"),
         EqualTo('password', message="Password Required")])
     submit_button = SubmitField('Create')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data)
+        if user_object:
+            raise ValidationError("Username has been taken")
